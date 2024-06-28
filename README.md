a89929295674ca990e85b28c094fd26a337ccac6
Tanvir Ahmed <t.a.prova@gmail.com>
2024-06-28 11:41:18 +0600

    Added the datetime

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LEETCODE
 I am making this repo to store my leetcode problems
 <br>Testing the first practice.
+<br>2024-06-28 11:41:08.209129 : Added the datetime
--- update_readme.py
+++ update_readme.py
@@ -1,7 +1,11 @@
+import datetime
+
 # Open the README.md file in append mode
 with open('README.md', 'a') as file:
     # Get input from the user
     line = input("Enter a line to append to README.md: ")
+
+    current_date_time = str(datetime.datetime.now())
     
     # Append the line to the file
-    file.write( "<br>" + line + '\n')
+    file.write( "<br>" +current_date_time + " : " + line + '\n')
