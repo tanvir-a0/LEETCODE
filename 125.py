19eaec4ab895b55242d64831eb77a9abeb9f231e
Tanvir Ahmed <66798561+tanvir-a0@users.noreply.github.com>
2024-07-09 18:54:37 +0000

    updating 125.py

--- 125.py
+++ 125.py
@@ -6,6 +6,8 @@ class Solution:
                 temp_str = temp_str + element
             if element >= "a" and element <= "z":
                 temp_str = temp_str + element
+            if element >= "0" and element <= "9":
+                temp_str = temp_str + element
         temp_str = temp_str.lower()
         for i in range(len(temp_str)):
             if temp_str[i] != temp_str[len(temp_str) - i - 1]:
