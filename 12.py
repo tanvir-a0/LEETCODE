048875e397f7983ffeaedabe21f2e357e4be3fd1
Tanvir Ahmed <66798561+tanvir-a0@users.noreply.github.com>
2024-09-27 03:05:55 +0600

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        number_roman_dict = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
+
+        result = ""
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while n <= num:
+                num = num - n
+                result = result + number_roman_dict[n]
+        return result
