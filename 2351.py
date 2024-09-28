a22201bf9e28d8cd3b357bcd01457a11f6ae4e2d
Tanvir Ahmed <66798561+tanvir-a0@users.noreply.github.com>
2024-09-28 07:50:22 +0600

    Update 2351.py

--- 2351.py
+++ 2351.py
@@ -1,6 +1,21 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        for i in range(len(s)-1):
-            if s[i] == s[i+1]:
-                return s[i]
-                
+        temp_dic = {
+
+        }
+        def check_dic(temp_dic = temp_dic):
+            for x in temp_dic.keys():
+                print(x)
+                if temp_dic[x] >= 2:
+                    return x
+            return None
+
+        for i in range(len(s)):
+            if s[i] in temp_dic:
+                temp_dic[s[i]] = temp_dic[s[i]] + 1
+            else:
+                temp_dic[s[i]] = 1
+
+            if check_dic(temp_dic) != None:
+                return check_dic(temp_dic)
+            print(temp_dic)
