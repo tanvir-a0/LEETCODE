df9e5624fb89a9c15877a3e89629ea0efca0e0b2
Tanvir Ahmed <66798561+tanvir-a0@users.noreply.github.com>
2024-10-24 23:57:08 +0600

    Create 217.py

---
+++ 217.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set(nums)
+
+        if len(s) == len(nums):
+            return False
+        return True
+        
