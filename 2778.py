30fdc7081532cd687564604d15d8521957cef264
Tanvir Ahmed <66798561+tanvir-a0@users.noreply.github.com>
2024-11-05 06:11:02 +0600

    Create 2778.py

---
+++ 2778.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(len(nums)):
+            if len(nums) % (i+1) == 0:
+                ans = ans + nums[i]**2
+        return ans
+        
