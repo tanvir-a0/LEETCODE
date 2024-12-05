f8df06f681a23aa31ff24c1235d8103e9a59451d
Tanvir Ahmed <66798561+tanvir-a0@users.noreply.github.com>
2024-12-05 12:55:46 +0600

    Update 914.py

--- 914.py
+++ 914.py
@@ -1,16 +1,18 @@
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        if len(deck) == 1:
+            return False
+
         dictt = {}
         for element in deck:
             if element in dictt:
                 dictt[element] = dictt[element] + 1
             else:
                 dictt[element] = 1
-            
-        maxx = max(list(dictt.values()))
-        minn = min(list(dictt.values()))
 
-        if maxx - minn:
-            return False
-        else:
-            return True
+        num = dictt[deck[0]]
+        for key, value in dictt.items():
+            num = math.gcd(num, value)
+
+
+        return num > 1
